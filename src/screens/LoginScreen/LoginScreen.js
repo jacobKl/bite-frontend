e95610812ea67b1f4fb1b2c8a4b9d3c9e1456188
jakubwiechniak <wiechniak.j@gmail.com>
2023-01-14 20:25:47 +0100

    fetch

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
-async function submitLogin(form) {
+async function submitLogin(e) {
+  e.preventDefault();
+  let form = e.target;
+  console.log(form)
   let formdata = new FormData(form);
-  console.log(formdata);
+  let result = await fetch("http://localhost:3001/user/login", {
+    method: "POST",
+    body: new URLSearchParams(formdata)
+  }).then(res => res.json())
+  console.log(result);
 }
 
 function LoginScreen() {
   return (
     <main className="w-100 h-100 d-flex align-items-center justify-content-center">
-      <form onSubmit={(e) => submitLogin(e.target)} class="d-flex flex-column paper text-center">
+      <form onSubmit={(e) => submitLogin(e)} className="d-flex flex-column paper text-center">
         <h3>Witaj!</h3>
         <input type="text" name="username" class="form-control mb-1" placeholder="Nazwa uzytkownika" />
         <input name="password" className="form-control mb-1" type="password" placeholder="Hasło     " />
